deedee4399579dbf2f01135ed343b90c875867c3
LRGBX <lvrg360x@gmail.com>
2019-06-07 02:39:33 +0300

    Custimizations
    Clear, renew, tweak

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,37 @@
 /**
+ * Gulp Packages
+ */
+
+// General
+const {src, dest, watch, series, parallel} = require('gulp');
+const del = require('del');
+const flatmap = require('gulp-flatmap');
+const lazypipe = require('lazypipe');
+const rename = require('gulp-rename');
+const header = require('gulp-header');
+const package = require('./package.json');
+
+// Scripts
+const babel = require('gulp-babel');
+const jshint = require('gulp-jshint');
+const stylish = require('jshint-stylish');
+const concat = require('gulp-concat');
+const uglify = require('gulp-terser');
+const optimizejs = require('gulp-optimize-js');
+
+// Styles
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
+const minify = require('gulp-cssnano');
+
+// SVGs
+const svgmin = require('gulp-svgmin');
+
+// BrowserSync
+const browserSync = require('browser-sync');
+
+
+/**
  * Settings
  * Turn on/off build features
  */
@@ -6,7 +39,7 @@
 var settings = {
 	clean: true,
 	scripts: true,
-	polyfills: true,
+	polyfills: false,
 	styles: true,
 	svgs: true,
 	copy: true,
@@ -22,7 +55,7 @@ var paths = {
 	input: 'src/',
 	output: 'dist/',
 	scripts: {
-		input: 'src/js/*',
+		input: ['src/js/*'],
 		polyfills: '.polyfill.js',
 		output: 'dist/js/'
 	},
@@ -46,165 +79,90 @@ var paths = {
  * Template for banner to add to file headers
  */
 
-var banner = {
-	full:
-		'/*!\n' +
-		' * <%= package.name %> v<%= package.version %>\n' +
-		' * <%= package.description %>\n' +
-		' * (c) ' + new Date().getFullYear() + ' <%= package.author.name %>\n' +
-		' * <%= package.license %> License\n' +
-		' * <%= package.repository.url %>\n' +
-		' */\n\n',
-	min:
-		'/*!' +
-		' <%= package.name %> v<%= package.version %>' +
-		' | (c) ' + new Date().getFullYear() + ' <%= package.author.name %>' +
-		' | <%= package.license %> License' +
-		' | <%= package.repository.url %>' +
-		' */\n'
-};
-
-
-/**
- * Gulp Packages
- */
-
-// General
-var {gulp, src, dest, watch, series, parallel} = require('gulp');
-var del = require('del');
-var flatmap = require('gulp-flatmap');
-var lazypipe = require('lazypipe');
-var rename = require('gulp-rename');
-var header = require('gulp-header');
-var package = require('./package.json');
-
-// Scripts
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
-var concat = require('gulp-concat');
-var uglify = require('gulp-terser');
-var optimizejs = require('gulp-optimize-js');
-
-// Styles
-var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
-var minify = require('gulp-cssnano');
-
-// SVGs
-var svgmin = require('gulp-svgmin');
-
-// BrowserSync
-var browserSync = require('browser-sync');
+// var banner = {
+// 	full:
+// 		'/*!\n' +
+// 		' * <%= package.name %> v<%= package.version %>\n' +
+// 		' * <%= package.description %>\n' +
+// 		' * (c) ' + new Date().getFullYear() + ' <%= package.author.name %>\n' +
+// 		' * <%= package.license %> License\n' +
+// 		' * <%= package.repository.url %>\n' +
+// 		' */\n\n',
+// 	min:
+// 		'/*!' +
+// 		' <%= package.name %> v<%= package.version %>' +
+// 		' | (c) ' + new Date().getFullYear() + ' <%= package.author.name %>' +
+// 		' | <%= package.license %> License' +
+// 		' | <%= package.repository.url %>' +
+// 		' */\n'
+// };
 
 
 /**
  * Gulp Tasks
  */
 
-// Remove pre-existing content from output folders
 var cleanDist = function (done) {
-
-	// Make sure this feature is activated before running
 	if (!settings.clean) return done();
-
-	// Clean the dist folder
 	del.sync([
 		paths.output
 	]);
-
-	// Signal completion
 	return done();
-
 };
 
-// Repeated JavaScript tasks
 var jsTasks = lazypipe()
-	.pipe(header, banner.full, {package: package})
+	// .pipe(header, banner.full, {package: package})
+	.pipe(babel, {presets: ['@babel/preset-env']})
 	.pipe(optimizejs)
 	.pipe(dest, paths.scripts.output)
 	.pipe(rename, {suffix: '.min'})
 	.pipe(uglify)
 	.pipe(optimizejs)
-	.pipe(header, banner.min, {package: package})
+	// .pipe(header, banner.min, {package: package})
 	.pipe(dest, paths.scripts.output);
 
-// Lint, minify, and concatenate scripts
 var buildScripts = function (done) {
-
-	// Make sure this feature is activated before running
 	if (!settings.scripts) return done();
-
-	// Run tasks on script files
 	return src(paths.scripts.input)
 		.pipe(flatmap(function(stream, file) {
-
-			// If the file is a directory
 			if (file.isDirectory()) {
-
-				// Setup a suffix variable
 				var suffix = '';
-
-				// If separate polyfill files enabled
 				if (settings.polyfills) {
-
-					// Update the suffix
 					suffix = '.polyfills';
-
-					// Grab files that aren't polyfills, concatenate them, and process them
 					src([file.path + '/*.js', '!' + file.path + '/*' + paths.scripts.polyfills])
 						.pipe(concat(file.relative + '.js'))
 						.pipe(jsTasks());
-
 				}
-
-				// Grab all files and concatenate them
-				// If separate polyfills enabled, this will have .polyfills in the filename
 				src(file.path + '/*.js')
 					.pipe(concat(file.relative + suffix + '.js'))
 					.pipe(jsTasks());
-
 				return stream;
-
 			}
-
-			// Otherwise, process the file
 			return stream.pipe(jsTasks());
-
 		}));
-
 };
 
 // Lint scripts
 var lintScripts = function (done) {
-
-	// Make sure this feature is activated before running
 	if (!settings.scripts) return done();
-
-	// Lint scripts
 	return src(paths.scripts.input)
 		.pipe(jshint())
 		.pipe(jshint.reporter('jshint-stylish'));
-
 };
 
 // Process, lint, and minify Sass files
 var buildStyles = function (done) {
-
-	// Make sure this feature is activated before running
 	if (!settings.styles) return done();
-
-	// Run tasks on all Sass files
 	return src(paths.styles.input)
 		.pipe(sass({
 			outputStyle: 'expanded',
 			sourceComments: true
 		}))
 		.pipe(prefix({
-			browsers: ['last 2 version', '> 0.25%'],
 			cascade: true,
 			remove: true
 		}))
-		.pipe(header(banner.full, { package : package }))
+		// .pipe(header(banner.full, { package : package }))
 		.pipe(dest(paths.styles.output))
 		.pipe(rename({suffix: '.min'}))
 		.pipe(minify({
@@ -212,62 +170,40 @@ var buildStyles = function (done) {
 				removeAll: true
 			}
 		}))
-		.pipe(header(banner.min, { package : package }))
+		// .pipe(header(banner.min, { package : package }))
 		.pipe(dest(paths.styles.output));
-
 };
 
 // Optimize SVG files
 var buildSVGs = function (done) {
-
-	// Make sure this feature is activated before running
 	if (!settings.svgs) return done();
-
-	// Optimize SVG files
 	return src(paths.svgs.input)
 		.pipe(svgmin())
 		.pipe(dest(paths.svgs.output));
-
 };
 
-// Copy static files into output folder
 var copyFiles = function (done) {
-
-	// Make sure this feature is activated before running
 	if (!settings.copy) return done();
-
-	// Copy static files
 	return src(paths.copy.input)
 		.pipe(dest(paths.copy.output));
-
 };
 
-// Watch for changes to the src directory
 var startServer = function (done) {
-
-	// Make sure this feature is activated before running
 	if (!settings.reload) return done();
-
-	// Initialize BrowserSync
 	browserSync.init({
 		server: {
 			baseDir: paths.reload
 		}
 	});
-
-	// Signal completion
 	done();
-
 };
 
-// Reload the browser when files change
 var reloadBrowser = function (done) {
 	if (!settings.reload) return done();
 	browserSync.reload();
 	done();
 };
 
-// Watch for changes
 var watchSource = function (done) {
 	watch(paths.input, series(exports.default, reloadBrowser));
 	done();
@@ -278,8 +214,6 @@ var watchSource = function (done) {
  * Export Tasks
  */
 
-// Default task
-// gulp
 exports.default = series(
 	cleanDist,
 	parallel(
@@ -291,8 +225,6 @@ exports.default = series(
 	)
 );
 
-// Watch and reload
-// gulp watch
 exports.watch = series(
 	exports.default,
 	startServer,
--- src/js/main/some-code.js
+++ src/js/main/some-code.js
@@ -1,4 +1,9 @@
-document.addEventListener('click', function (event) {
-	if (!event.target.matches('#click-me')) return;
-	alert('You clicked me!');
-}, false);
+// document.addEventListener('click', function (event) {
+// 	if (!event.target.matches('#click-me')) return;
+// 	alert('You clicked me!');
+// }, false);
+
+var button = document.getElementById('click-me');
+button.addEventListener('click', function () {
+	alert('Mine Func!!!');
+});
